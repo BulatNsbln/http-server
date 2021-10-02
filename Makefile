bf797160f9e0cf3ea986f67bf81c87e64c21ff65
Bulat Nasibullin <nasibullinbulat95@gmail.com>
2021-10-02 17:09:16 +0400

    feat: add exercise 8

---
+++ Makefile
@@ -0,0 +1,8 @@
+console:
+	@node-console
+
+start:
+	nodemon --exec bin/phonebook.js
+test:
+	npm test -s
+
---
+++ user.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+let id = 1000;
+
+export const nextId = () => {
+  id += 1;
+  return id;
+};
+
+export const validate = ({ name, phone }) => {
+  // BEGIN (write your solution here)
+
+  // END
+};
